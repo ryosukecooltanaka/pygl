0f1d5151ba1ba0593e09bf268bdfc9345bb0f1cf
Ryosuke Tanaka <ryosuke.tanaka@yale.edu>
2022-02-24 12:33:15 -0500

    more comment

--- try3d.py
+++ try3d.py
@@ -2,13 +2,14 @@
 # https://noobtuts.com/python/opengl-introduction
 # the whole thing is very sensitive to variable type
 
+# import stuff
 from OpenGL.GL import *
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
 import numpy as np
 
-window = 0
-width, height = 500, 500
+# parameters
+width, height = 500, 500 # window size
 t = 0.0 # just a timestamp to change things
 
 # define the parameter of cubes
@@ -17,39 +18,46 @@ cube_angles   = np.random.rand(nCube,1) * 360
 cube_axes     = np.random.rand(nCube,3)
 cube_position = 10*(np.random.rand(nCube,3) - 0.5)
 
-
+# main callback function
 def draw():
+    # access time stamp variable t as a global variable
+    # otherwise any change you apply to t will not be propagated outside the
+    # function (you are operating on the local copy)
     global t
+
+    # enable depth rendering
     glEnable(GL_DEPTH_TEST)
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT) # clear buffer (black screen now)
-    glLoadIdentity() # reset position
+
+    # clear buffer (black screen now)
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
+    # load identity matrix
+    glLoadIdentity()
 
     # define camera
     refresh(width,height)
+
+    # update the timestamp to move things
     t += 0.01
 
     # draw cube
+    # make sure we are opearting on the modelview matrix
+    # this already has the camera matrix defined in refresh()
     glMatrixMode(GL_MODELVIEW)
     for i in range(nCube):
+        # save the current matrix
         glPushMatrix()
-
+        # move and rotate the coordinate for each cube
+        # translation comes first so the thing rotates on the spot
         glTranslated(cube_position[i,0], cube_position[i,1], cube_position[i,2]) # move
         glRotatef(cube_angles[i] + t*50, cube_axes[i,0], cube_axes[i,1], cube_axes[i,2]) # rotate
         draw_cube()
+        # bring back the original coordinate before the translation and rotation
         glPopMatrix()
-
-
     glutSwapBuffers() # this is like Flip in PTB?
 
-
-def draw_rect(x, y, z, rw, rh):
-    glBegin(GL_QUADS) # tell OpenGL we want a rectangle
-    glVertex3f(x, y, z) # define vertices in a counterclockwise order
-    glVertex3f(x+rw, y, z)
-    glVertex3f(x+rw, y+rh, z)
-    glVertex3f(x, y+rh, z)
-    glEnd()
-
+# Draw a cube with different side colors and the edge length = 2
+# centered at the origin
 def draw_cube():
     glBegin(GL_QUADS) # I am drawing a bunch of rectangles
 
@@ -91,32 +99,37 @@ def draw_cube():
 
     glEnd()
 
-
-
-
-# Updates the camera
+# This function defines the camera and projection
 def refresh(w, h):
-    glViewport(0, 0, w, h) # map projected image ranging from -1 to 1 to the actual screen
-    glMatrixMode(GL_PROJECTION) # tell open GL we are definying projction matrix
-    glLoadIdentity()
+    # define the viewport transform
+    # this maps NDC cube ranging from [-1,1] to actual screen coordinate
+    # nothing interesting happens here
+    glViewport(0, 0, w, h)
+
+
+    # define the projection (use the perspective projection)
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity() # intialize by loading identity
     # Define a frustum for a perspective projection
     # Position of four edges of the near plane + Z-position of near and far
     # planes (camera is supposed to be at the origin)
+    # numbers are in the model coordinate (but after all rotations etc)
     glFrustum(-1, 1, -1, 1, 2, 30.0)
 
-    glMatrixMode(GL_MODELVIEW) # tell open GL we are definying modelview matrix
-    glLoadIdentity()
+    # define the camera
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity() # intialize by loading identity
     gluLookAt(20*np.cos(t), 0, 20*np.sin(t),  # where your eye is
-              0,0,0,  # where you are looking at
-              0.0, 1.0, 0.0)  # which way is up
+              0,0,0,                          # where you are looking at
+              0.0, 1.0, 0.0)                  # which way is up
 
 
 
-glutInit()
-glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_ALPHA | GLUT_DEPTH)
-glutInitWindowSize(width, height)
-glutInitWindowPosition(0, 0)
-win = glutCreateWindow(b"my window") # only takes char = byte data
+glutInit() # initialize opengl
+glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_ALPHA | GLUT_DEPTH) # define the display mode
+glutInitWindowSize(width, height) # set the window size
+glutInitWindowPosition(0, 0) # set the window position
+win = glutCreateWindow(b"my window") # create a window with a title (only takes char = byte data hence b)
 glutDisplayFunc(draw) # setting a callback (this happens everytime the user interacts with the window)
 glutIdleFunc(draw) # draw all the time
 glutMainLoop()
